66209cda9a1efa7ec1571978c4c53ccf1cf8995c
Ramit Sarkar <ramitsarkar902@gmail.com>
2023-03-31 23:28:37 +0530

    login using redux -->ramit

---
+++ server/models/Student.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const studentSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    branch: {
+      type: String,
+    },
+    section: {
+      type: String,
+    },
+    regNo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Student", studentSchema);
